b8f3a2a284a59357c192961491c9935737b7778b
fruitea <59316330+aetiurf@users.noreply.github.com>
2022-11-04 13:16:40 +0000

    update

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,29 +8,32 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
     Creation(CreationError),
-    ParseInt(ParseIntError)
+    ParseInt(ParseIntError),
 }
 
-impl ParsePosNonzeroError {
-    // TODO: add another error conversion function here.
+impl From<CreationError> for ParsePosNonzeroError {
+    fn from(value: CreationError) -> Self {
+        Self::Creation(value)
+    }
 }
 
-fn parse_pos_nonzero(s: &str)
-    -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
-{
+impl From<ParseIntError> for ParsePosNonzeroError {
+    fn from(value: ParseIntError) -> Self {
+        Self::ParseInt(value)
+    }
+}
+
+fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+    let x: i64 = s.parse()?;
+    Ok(PositiveNonzeroInteger::new(x)?)
 }
 
 // Don't change anything below this line.
@@ -49,7 +52,7 @@ impl PositiveNonzeroInteger {
         match value {
             x if x < 0 => Err(CreationError::Negative),
             x if x == 0 => Err(CreationError::Zero),
-            x => Ok(PositiveNonzeroInteger(x as u64))
+            x => Ok(PositiveNonzeroInteger(x as u64)),
         }
     }
 }
